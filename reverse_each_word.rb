dbb70632bddde70ef1605b407aa7c1a9c462f539
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-11 21:35:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 message = ("Hello there, and how are you?")
 def reverse_each_word(string)
   new_array = string.split
-  new_array.collect{|word| word.reverse}
+  new_string = new_array.collect{|word| word.reverse}
+  new_string.join(" ")
 end
 reverse_each_word(message)
